c2d62e0c91ee9cd40c0ba45bc4136f7a2029b44f
Kuznetchenko <andreytwonly@gmail.com>
2023-03-29 19:48:31 +0500

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Andrey Kuznetsov");
         System.out.println("24.08.2001");
     }
