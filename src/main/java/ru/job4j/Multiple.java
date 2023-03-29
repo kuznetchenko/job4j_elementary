73bea56cecd626a10320ae312ef665cbeba29236
Kuznetchenko <andreytwonly@gmail.com>
2023-03-29 19:20:13 +0500

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args){
+
         System.out.println("1 * 2 = 2");
         System.out.println("1 * 3 = 3");
         System.out.println("1 * 4 = 4");
@@ -10,5 +11,6 @@ public class Multiple {
         System.out.println("1 * 7 = 7");
         System.out.println("1 * 8 = 8");
         System.out.println("1 * 9 = 9");
+
     }
 }
